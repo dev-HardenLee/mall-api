4d5ebcbf27709258ce14ac6fe85a3870b95da979
harden <harden.lee@companywe.co.kr>
2024-03-20 14:39:48 +0900

    Todo API 개발완료, LocalDateFormatter 추가, CORS 설정 추가.

--- src/main/java/org/zerock/mallapi/controller/TodoController.java
+++ src/main/java/org/zerock/mallapi/controller/TodoController.java
@@ -2,14 +2,13 @@ package org.zerock.mallapi.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.zerock.mallapi.dto.PageRequestDTO;
 import org.zerock.mallapi.dto.PageResponseDTO;
 import org.zerock.mallapi.dto.TodoDTO;
 import org.zerock.mallapi.service.TodoService;
+
+import java.util.Map;
 
 @RestController
 @Log4j2
@@ -30,6 +29,31 @@ public class TodoController {
 
         return todoService.getList(pageRequestDTO);
     }// list
+
+    @PostMapping("/")
+    public Map<String, Long> register(@RequestBody TodoDTO todoDTO) {
+        log.info("todoDTO: " + todoDTO);
+
+        Long tno = todoService.register(todoDTO);
+
+        return Map.of("tno", tno);
+    }// register
+
+    @PutMapping("/{tno}")
+    public Map<String, String> modify(@PathVariable Long tno, @RequestBody TodoDTO todoDTO) {
+        todoDTO.setTno(tno);
+
+        todoService.modify(todoDTO);
+
+        return Map.of("result", "success");
+    }// modify
+
+    @DeleteMapping("/{tno}")
+    public Map<String, String> remove(@PathVariable Long tno) {
+        todoService.remove(tno);
+
+        return Map.of("result", "success");
+    }// modify
 
 }// TodoController
 
