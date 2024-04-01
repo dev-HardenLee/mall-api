f6d4f5a0a2de0dc004b2557bb563c8875cb668c4
harden <harden.lee@companywe.co.kr>
2024-04-01 11:07:27 +0900

    상품등록 entity 처리.

---
+++ src/main/java/org/zerock/mallapi/domain/Product.java
@@ -0,0 +1,62 @@
+package org.zerock.mallapi.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tbl_product")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString(exclude = {"imageList"})
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pno;
+
+    private String pname;
+
+    private int price;
+
+    private String pdesc;
+
+    private boolean delFlag;
+
+    @ElementCollection
+    @Builder.Default
+    private List<ProductImage> imageList = new ArrayList<>();
+
+    public void changeName(String pname) {
+        this.pname = pname;
+    }
+
+    public void changePrice(int price) {
+        this.price = price;
+    }
+
+    public void changePdesc(String pdesc) {
+        this.pdesc = pdesc;
+    }
+
+    public void addImage(ProductImage image) {
+        image.setOrd(imageList.size());
+
+        imageList.add(image);
+    }// addImage
+
+    public void addImageString(String fileName) {
+        ProductImage productImage = ProductImage.builder().fileName(fileName).build();
+
+        addImage(productImage);
+    }// addImageString
+
+    public void clearList() {
+        this.imageList.clear();
+    }// clearList
+
+}
